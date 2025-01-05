277b1a88b947089cbbabe7d5ef8799819398da27
Nabeeha <nabeehaghani@gmail.com>
2025-01-04 22:47:23 -0600

    new solution

--- 8kyu/reversed-sequence.js
+++ 8kyu/reversed-sequence.js
@@ -1,6 +1,6 @@
-// ==========================
+// ===========================
 // INSTRUCTIONS
-// ==========================
+// ===========================
 
 // Build a function that returns an array of integers from n to 1 where n>0.
 
