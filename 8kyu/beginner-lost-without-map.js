dd542a95d9d25e69acf854a6535df017a603420d
Nabeeha <nabeehaghani@gmail.com>
2025-01-07 09:57:25 -0600

    new solution

---
+++ 8kyu/beginner-lost-without-map.js
@@ -0,0 +1,23 @@
+// ==========================
+// INSTRUCTIONS
+// ==========================
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// ==========================
+// MY SOLUTION
+// ==========================
+
+function maps(x){
+    return x.map(s => s * 2)
+  }
+
+// ==========================
+// OTHER SOLUTIONS
+// ==========================
+
+maps = x => x.map(e => e * 2);
