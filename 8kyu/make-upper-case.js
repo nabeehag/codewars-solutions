fa7a5e222a377c5c2644942a2173f9801b7467ee
Nabeeha <nabeehaghani@gmail.com>
2025-01-09 23:37:43 -0600

    new solution

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,13 @@
+// ==========================
+// INSTRUCTIONS
+// ==========================
+
+// Write a function which converts the input string to uppercase.
+
+// ==========================
+// MY SOLUTION
+// ==========================
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
