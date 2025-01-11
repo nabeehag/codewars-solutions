78e639ac8a36ad92cfc3b4e25d9fe8fdb368a217
Nabeeha <nabeehaghani@gmail.com>
2025-01-10 23:28:44 -0600

    new solution

---
+++ 8kyu/type-of-sum.js
@@ -0,0 +1,19 @@
+// ==========================
+// INSTRUCTIONS
+// ==========================
+
+// Return the type of the sum of the two arguments
+
+// ==========================
+// MY SOLUTION
+// ==========================
+
+function typeOfSum(a, b) {
+    return typeof (a + b)
+  }
+
+// ==========================
+// OTHER SOLUTIONS
+// ==========================
+
+const typeOfSum = (a, b) => typeof(a + b);
