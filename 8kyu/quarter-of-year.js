007f96e40c88c6e974efcebdc0b1857a68b43884
Nabeeha <nabeehaghani@gmail.com>
2025-01-13 17:04:36 -0600

    new solution

--- 8kyu/quarter-of-year.js
+++ 8kyu/quarter-of-year.js
@@ -2,13 +2,19 @@
 // INSTRUCTIONS
 // ==========================
 
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// Constraint:
+
+// 1 <= month <= 12
 
 // ==========================
 // MY SOLUTION
 // ==========================
 
-const quarterOf = (month) => {
+let quarterOf = (month) => {
     return ((month >= 1 && month <= 3) ? 1 : 
            (month >= 4 && month <= 6) ? 2 : 
            (month >= 7 && month <= 9) ? 3 : 
@@ -19,4 +25,5 @@ const quarterOf = (month) => {
 // OTHER SOLUTIONS
 // ==========================
 
+let quarterOf = m => Math.ceil(m/3);
 
