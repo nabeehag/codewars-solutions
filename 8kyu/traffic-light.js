ab613a316e693a02651ed1f0a612c6c94330a0c7
Nabeeha <nabeehaghani@gmail.com>
2025-01-13 17:03:40 -0600

    new solution

--- 8kyu/traffic-light.js
+++ 8kyu/traffic-light.js
@@ -2,7 +2,11 @@
 // INSTRUCTIONS
 // ==========================
 
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, when the input is green, output should be yellow.
 
 // ==========================
 // MY SOLUTION
@@ -16,4 +20,8 @@ function updateLight(current) {
 // OTHER SOLUTIONS
 // ==========================
 
-
+const updateLight = current => ({
+    green: 'yellow',
+    yellow: 'red',
+    red: 'green',
+  })[current]
