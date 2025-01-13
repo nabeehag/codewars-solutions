f625faf285c7ad19e327c60d1056b2979f313bce
Nabeeha <nabeehaghani@gmail.com>
2025-01-13 16:58:38 -0600

    new solution

--- 7kyu/string-ends-with.js
+++ 7kyu/string-ends-with.js
@@ -2,7 +2,12 @@
 // INSTRUCTIONS
 // ==========================
 
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
 
 // ==========================
 // MY SOLUTION
@@ -16,4 +21,5 @@ function solution(str, ending){
 // OTHER SOLUTIONS
 // ==========================
 
+const solution = (str, ending) => str.endsWith(ending);
 
