1609a9d1da3b3b1a8319f16f28924f5957c57984
Nabeeha <nabeehaghani@gmail.com>
2025-01-30 17:00:14 -0600

    new solution

---
+++ 8kyu/five-without-numbers.js
@@ -0,0 +1,25 @@
+// ==========================
+// INSTRUCTIONS
+// ==========================
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// ==========================
+// MY SOLUTION
+// ==========================
+
+function unusualFive() {
+    return ['a', 'b', 'c', 'd', 'e'].length
+  }
+
+// ==========================
+// OTHER SOLUTIONS
+// ==========================
+
+function unusualFive() {
+    return "hello".length;
+  }
