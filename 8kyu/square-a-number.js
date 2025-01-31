65cb50e1ce02ea3256e2a0725c59bff91c71c191
Nabeeha <nabeehaghani@gmail.com>
2025-01-31 17:50:29 -0600

    new solution

---
+++ 8kyu/square-a-number.js
@@ -0,0 +1,18 @@
+// ==========================
+// INSTRUCTIONS
+// ==========================
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// ==========================
+// MY SOLUTION
+// ==========================
+
+let square = num => num ** 2
+
+// ==========================
+// OTHER SOLUTIONS
+// ==========================
+
+let square = (n) => n * n;
+
