8148b64af70e504881affabacf096d9a3042ec25
Nabeeha <nabeehaghani@gmail.com>
2025-02-15 23:43:14 -0600

    new solution

---
+++ boolean-to-string.js
@@ -0,0 +1,15 @@
+// ==========================
+// INSTRUCTIONS
+// ==========================
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// ==========================
+// MY SOLUTION
+// ==========================
+
+function booleanToString(b){
+    return b.toString();
+}
