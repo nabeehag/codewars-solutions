75034a5f95e26ec07643c6e8499bd7672efca142
Nabeeha <nabeehaghani@gmail.com>
2025-03-03 23:50:32 -0600

    new solution

--- 7kyu/form-minimum.js
+++ 7kyu/form-minimum.js
@@ -1,6 +1,6 @@
 // ==========================
 // INSTRUCTIONS
-// ==========================
+// ==========================  
 
 // Task
 // Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers in the range of 1 to 9 will be passed to the function.
