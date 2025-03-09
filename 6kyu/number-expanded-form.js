7a036d60ad1b9a987c2811589ba4db079ea2ef8f
Nabeeha <nabeehaghani@gmail.com>
2025-03-08 22:19:42 -0600

    new solution

--- 6kyu/number-expanded-form.js
+++ 6kyu/number-expanded-form.js
@@ -1,5 +1,5 @@
 // ==========================
-// INSTRUCTIONS
+// INSTRUCTIONS  
 // ==========================
 
 // You will be given a number and you will need to return it as a string in Expanded Form. For example:
