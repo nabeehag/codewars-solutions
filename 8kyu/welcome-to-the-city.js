7a11076aa326f6bea9a0a4a5c52174d272ec1fa2
Nabeeha <nabeehaghani@gmail.com>
2025-03-14 23:56:53 -0500

    new solution

--- 8kyu/welcome-to-the-city.js
+++ 8kyu/welcome-to-the-city.js
@@ -2,7 +2,7 @@
 // INSTRUCTIONS
 // ==========================
 
-// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+// Create a method that takes the input of a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
 
 // Example:
 
